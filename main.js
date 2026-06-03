35ee39b75fb14452e9b1111e62d1b4f17c697a20
Farias Yesica <yesicafarias1522@gmail.com>
2026-06-03 17:42:32 -0300

    feat: add search validation and Enter key support

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 import { buscarPeliculas } from "./api.js";
 import { mostrarPeliculas } from "./ui.js";
 
-const boton =
-  document.getElementById("buscar");
+const boton = document.getElementById("buscar");
+const inputBusqueda = document.getElementById("busqueda");
+const resultado = document.getElementById("resultado");
 
 boton.addEventListener("click", async () => {
-  const resultado =
-    document.getElementById("resultado");
 
+const titulo = inputBusqueda.value.trim();
+ if (titulo === "") {
   resultado.innerHTML =
-    "<p>Cargando películas...</p>";
+    "<p style='color: red;'>Por favor, escribe el nombre de una película.</p>";
 
-  const texto =
-    document.getElementById("busqueda").value;
+  inputBusqueda.focus();    //devuelve el foco al input automáticamente para que el usuario escriba nuvamente.
 
-  const datos =
-    await buscarPeliculas(texto);
+  return;
+}
+  resultado.innerHTML = "<p>Cargando películas...</p>";
+
+  const texto = inputBusqueda.value;
+  
+  const datos = await buscarPeliculas(texto);
 
   mostrarPeliculas(datos);
 });
 
+inputBusqueda.addEventListener("keydown", (event) => {          // Escucha cada tecla que presionas dentro del imput.
+  if (event.key === "Enter") {                                  // Cuando la tecla es enter: simula un click sobre el botón.
+    boton.click();
+  }
+});
